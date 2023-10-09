9f7f91bdb4afa808378deca6b63ebd2fd4fc31ee
Your Name <Immanuel Leo Praksh S>
2023-10-09 18:33:48 +0530

    Show the spinner while fetching the genres

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,26 +1,37 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../Hooks/useGenres";
 import getCropperImageUrl from "../Services/image-url";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const GenreListSkeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  if (error) {
+    return null;
+  }
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCropperImageUrl(genre.image_background)}
-            />
-            <Text fontSize="lg" key={genre.id}>
-              {genre.name}
-            </Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCropperImageUrl(genre.image_background)}
+              />
+              <Text fontSize="lg" key={genre.id}>
+                {genre.name}
+              </Text>
+            </HStack>
+            {/* {isLoading&& <GenreListSkeleton/>} */}
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
---
+++ src/Components/GenreListSkeleton.tsx
@@ -0,0 +1,23 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Skeleton,
+  SkeletonCircle,
+  SkeletonText,
+} from "@chakra-ui/react";
+import React from "react";
+
+const GenreListSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GenreListSkeleton;
