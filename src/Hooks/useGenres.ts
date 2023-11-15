7ac75449b66d937b934903378a6cdabe5fc94925
Your Name <Immanuel Leo Praksh S>
2023-11-15 14:20:52 +0530

    import clean up

--- src/Hooks/useGenres.ts
+++ src/Hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../Data/Genre";
-import APIClient, { FetchResponse } from "../Services/api-client";
+import APIClient from "../Services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 
