3d1cdc54586de50f234c9ee11e8f24fc0b75fe5a
Your Name <Immanuel Leo Praksh S>
2023-11-23 15:26:53 +0530

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Layout from "./Pages/Layout";
 import GamesDetailPage from "./Pages/GamesDetailPage";
+import ErrorPage from "./Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage/>,
     children: [
       {
         index: true,
